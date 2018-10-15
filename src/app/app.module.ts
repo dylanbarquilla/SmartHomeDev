4fe1836cc51fc264cd05452e5089177fa4d07751
MathisDELOGE <thisdemalo@gmail.com>
2018-10-15 15:59:07 +0200

    ajout component toolbar-menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { MaterialModule} from './material';
+import { ToolbarMenuComponent } from './toolbar-menu/toolbar-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarMenuComponent
   ],
   imports: [
     BrowserModule,
